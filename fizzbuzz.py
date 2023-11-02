7cf671d2a8c1e7018cfcb50a52875734f4eb8006
Ashley Castle <ashleycastle2010@gmail.com>
2023-11-02 19:28:40 -0400

    FizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,10 @@
+
 for num in range(1,101):
-    if num % 3 == 0:
-        print("Fizz")
-    if num % 5 == 0:
-        print("Buzz")
-    if num % 5 == 0 and num % 3 == 0:
-        print("FizzBuzz")
-    if num % 5 != 0 and num % 3 != 0:
-        print(num)
+  if num % 5 == 0 and num % 3 == 0:
+    print("FizzBuzz")
+  elif num % 5 == 0:
+    print("Buzz")
+  elif num % 3 == 0:
+    print("Fizz")
+  else:
+    print(num)
